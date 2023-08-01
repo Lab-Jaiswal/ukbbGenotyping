b8013748ce8a8996e905f01fdb5ad817442891fb
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2023-08-01 14:31:53 -0700

    Update example_run_through.R

--- example_run_through.R
+++ example_run_through.R
@@ -1,31 +1,21 @@
 #install.packages("devtools")
 library(devtools)
-library(GenomicRanges)
-library(readxl)
 library(magrittr)
 library(tidyverse)
 library(pgenlibr)
-library(ukbtools)
-library(lubridate)
-devtools::install_github("Lab-Jaiswal/topmed_genotyping")
-library(topmedGenotyping)
+#devtools::install_github("Lab-Jaiswal/ukbbGenotyping")
+library(ukbbGenotyping)
 
-#Hanscombe KB, Coleman J, Traylor M, Lewis CM (e-print 158113). “ukbtoo
-#UK Biobank data.” _bioRxiv_. <URL: https://doi.org/10.1101/158113>.
-
-directory <- "/oak/stanford/projects/topmed/topmed_f9/genotypes/pgen_files/"
+directory <- "/oak/stanford/groups/sjaiswal/maurertm/ukb_genetic_files/22828_files/22828_pgens_Apr_11/"
 rsid_fp <- read_tsv("/oak/stanford/projects/topmed/topmed_f9/genotypes/pgen_files/chip_snps_vep_annotated_rsid.tsv", col_names = FALSE)
 rsids <- rsid_fp %>% pull(X5)
 
-sequnce <- seq(1:22) #%>% append(c("X"))
-chr_list<- sprintf("chr%s", sequnce) 
+sequnce <- seq(1:22) %>% append(c("X"))
+chr_list<- sprintf("c%s", sequnce) 
 
-pvar <- make_pvar("chr1", directory)
-
-variants_df <- get_variants(chr_list[21:22], rsids, directory) %>% filter(!is.na(index)) 
-
+variants_df <- get_variants(chr_list[21:22], rsids, directory) 
 pgen_list <- get_pgen_list(chr_list[21:22], directory)
-`
-genotypes <- get_all_genotypes(variants_df, pgen_list)
-write_rds(geno_full, "ukbiobank_genotypes.rda")
 
+all_genotypes <- get_all_genotypes(variants_df, pgen_list, directory)
+
+write_rds(geno_full, "ukbiobank_genotypes.rda")
