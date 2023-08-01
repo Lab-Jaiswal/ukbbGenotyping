1461237fac66d78780f3a2aa7582f6b737d35c6c
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2023-08-01 15:19:30 -0700

    Update get_genotypes.R

--- R/get_genotypes.R
+++ R/get_genotypes.R
@@ -11,7 +11,7 @@ get_genotypes <- function(variants_df, directory, psam){
   
   genotypes <- group_by(variants_df, chromosome) %>% 
     group_map(get_genotypes_from_pgen, pgen_list) %>% 
-    bind_cols
+    bind_cols %>% as.data.frame()
   rownames(genotypes) <- psam$IID
   genotypes_df <- as_tibble(genotypes, rownames = "eid")
 }
